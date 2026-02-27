c70877e4c2c10d62d4de7baeeb8ba01ebef6dca5
Diar <you@example.com>
2026-02-27 11:39:30 +0500

    deleted files

--- src/core/dependencies/exceptions.py
+++
@@ -1,7 +0,0 @@
-from fastapi import status, HTTPException
-
-
-file_isnt_suported = HTTPException(
-    status_code=status.HTTP_400_BAD_REQUEST,
-    detail="Данный файл не поддерживается, файл должен иметь другое окончания"
-)
--- src/schemas/jobs.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, model_validator
-
-
-class JobResponse(BaseModel):
-    filename: str
-    contents: str
-    content_type: str
